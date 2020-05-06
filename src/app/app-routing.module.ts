480164c440d964efaed946b20fe755fa297d938e
medmsd <mseddi.mohamed@iit.ens.tn>
2020-05-06 15:29:22 +0100

    fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,21 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginComponent} from './core/login/login.component';
+import {LayoutPageComponent} from './core/layout-page/layout-page.component';
 
 
 const routes: Routes = [
-  {path: '',pathMatch:"full", redirectTo: 'tasks'},
-  {path: 'users', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)},
-  {path: 'projects', loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule)},
-  {path: 'tasks', loadChildren: () => import('./modules/tasks/tasks.module').then(m => m.TasksModule)},
-  {path: 'stats', loadChildren: () => import('./modules/stats/stats.module').then(m => m.StatsModule)},
-  {path:"login",redirectTo: "/(login:login)",pathMatch:"full"},
-  {path:"**",redirectTo: "tasks",pathMatch: "full"},
-  {path: 'login',outlet:"login", component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {
+    path: '', component: LayoutPageComponent, children: [
+      {path: '', pathMatch: 'full', redirectTo: 'tasks'},
+      {path: 'users', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule)},
+      {path: 'projects', loadChildren: () => import('./modules/projects/projects.module').then(m => m.ProjectsModule)},
+      {path: 'tasks', loadChildren: () => import('./modules/tasks/tasks.module').then(m => m.TasksModule)},
+      {path: 'stats', loadChildren: () => import('./modules/stats/stats.module').then(m => m.StatsModule)},
+      {path: '**', redirectTo: 'tasks', pathMatch: 'full'},
+    ]
+  }
 ];
 
 @NgModule({
